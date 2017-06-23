8f8ce31444f4ad4d8d0f2bd1d338f36df3ce7c0a
Cenethua <Selenaminez@gmail.com>
2017-06-23 11:22:54 -0700

    Finished with remake of high low game

--- src/HighLowGame.java
+++ src/HighLowGame.java
@@ -1,4 +1,5 @@
 
+import java.util.Random;
 import javax.swing.JOptionPane;
 
 /*
@@ -12,5 +13,31 @@ import javax.swing.JOptionPane;
  * @author Selena Minez
  */
 public class HighLowGame {
+   public  static void main(String[]args){
+       // Random number 
+       int random = new Random().nextInt(100)+1;
+       System.out.println(random);
        
+       for(int i = 0 ; i<3; i++){
+      //Question + conversion  
+       String answer = JOptionPane.showInputDialog("Guess a number between 1 to 100");
+       
+       int num = Integer.parseInt(answer);
+       
+       // If statments that determine what shows up after number is typed in 
+       if(num == random) {
+       JOptionPane.showMessageDialog(null,"You win!");
+       
+       } 
+       else if(num > random){
+       JOptionPane.showMessageDialog(null,"Too high");
+       
+       }
+       else if (num < random){
+       JOptionPane.showMessageDialog(null,"Too low");
+       }
+       
+       } 
+       JOptionPane.showMessageDialog(null,"You lose little pleb");
+   }  
 }
