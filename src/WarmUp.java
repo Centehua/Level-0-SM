46954dd84e73cc2662c340933df21a9072878fad
Centehua <league@WorthyMini-6.attlocal.net>
2017-08-04 16:56:28 -0700

    % WarmUp and FizzBuzz

---
+++ src/WarmUp.java
@@ -0,0 +1,28 @@
+
+public class WarmUp {
+
+	public static void main(String[] args) {
+		// XOXO
+		for (int i = 0; i <= 100; i++) {
+		if(i%2==0){
+		System.out.println("x");	
+		} else if (i%2==1){
+	    System.out.println("o");
+		} 
+		// Justin Bieber Song
+		} for (int i = 0; i <= 7; i++) {
+		 if(i==3){
+		 System.out.println("oh");
+		 }
+		 else if(i== 7){
+		 System.out.println("no");	 
+		 } 
+		 else{
+		System.out.println("baby"); 
+		 }
+		}
+		
+		
+		
+}
+} 
