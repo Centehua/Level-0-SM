99237375acc4d20bd76d424ae5cadb94487b8a56
Centehua <league@WorthyMini-6.attlocal.net>
2017-08-18 17:00:08 -0700

    ForLoop

---
+++ src/ForLoopGauntlet.java
@@ -0,0 +1,64 @@
+
+public class ForLoopGauntlet {
+	public static void main(String[] args) {
+//		for (int i = 0; i <=100; i++) {
+//			System.out.println(i);
+//		}
+//	for (int i = 100; i >= 0; i--) {
+//		System.out.println(i);
+//	}
+//	for (int i = 2; i <=100; i+=2) {
+//		System.out.println(i);
+//	}
+//	for (int i = 0; i <= 99; i++) {
+//		if(i%2==1) {
+//		System.out.println(i);		
+//		}
+//	}
+//	for (int i = 1; i <= 500; i++) {
+//		
+//		if(i%2==1) {
+//			System.out.println(i+" "+"odd");
+//			
+//		}else if(i%2==0) {
+//		System.out.println(i+" "+"even");	
+//		} 
+//	}
+//		for (int i = 0; i <= 777; i++) {
+//			if(i%7==0) {
+//			System.out.println(i);	
+//			}
+//		}
+//	for (int i = 14; i>=0 ; i--) { 
+//		
+//		System.out.println("In"+" " +(2017-i)+" I was"+" "+ (14-i) +" "+"years old");
+//		
+//		
+//	}
+//		for (int i = 0; i < 3; i++) {
+//			
+//			for (int j = 0; j < 3; j++) {
+//			System.out.println(i+""+j);
+//			
+//			}
+//			
+//			}
+//		for (int i = 1; i <= 100; i+=10) {
+//			for (int j = i; j <i+10 ; j++) {
+//				System.out.print(j+"\t");
+//				
+//			}
+//			System.out.println();
+//		}
+		for (int i = 0; i < 6; i++) {
+			
+			for (int j = 0; j < i; j++ ) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+			
+		}
+	}
+
