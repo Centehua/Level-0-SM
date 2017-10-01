c4bf71583a287d4c565681fe4eeaf5cf46701a66
Cenethua <Selenaminez@gmail.com>
2017-09-30 21:16:59 -0700

    Completed Fibonacci Sequence
    Finally understood my code.

--- src/FibonacciSequence.java
+++ src/FibonacciSequence.java
@@ -1,11 +1,50 @@
 
 public class FibonacciSequence {
+
+//    private static int fNum;
+    
  public static void main(String[] args) {
-	int fNum;
-	int sNum;
-	for (int i = 0; i <=200; i++) {
-	
-	System.out.println(i);
-	}
+     
+     
+     int k, a=1, b=0;
+     for (int i = 0; i < 10; i++) {
+         
+     
+     k=0;
+     k=a+b;
+     a=b;
+     b=k;
+     System.out.println(k);
+     }
+// int index =0;
+// while(true){
+// System.out.println(fibonacci(index));
+// index++;
+//   int num =0;
+//   int num2 = 1;
+//   int loop;
+//   int fibonacci;
+   
+//    System.out.println(num);      
+////     for (loop= 0; loop <10; loop++) {
+//       fibonacci= num + num2;
+//      num =num2;
+//      num2 = fibonacci;
+//       System.out.println(fibonacci);
+////     }
+//    
+        
+        }
+ 
+
+
+// public static long fibonacci(int i ){
+// if(i==0) return 0;
+//  if(i<=2) return 1;
+// 
+//  long fibTerm = fibonacci(i-1)+ fibonacci(i-2);
+//   return fibTerm;
+//    }    
+      
 }
-}
+
