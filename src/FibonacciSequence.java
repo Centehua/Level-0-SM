52631a53f6fa10919d1b7761c5d2623b2a92fd91
Cenethua <Selenaminez@gmail.com>
2017-10-15 17:45:06 -0700

    Small changes

--- src/FibonacciSequence.java
+++ src/FibonacciSequence.java
@@ -6,45 +6,39 @@ public class FibonacciSequence {
  public static void main(String[] args) {
      
      
-     int k, a=1, b=0;
-     for (int i = 0; i < 10; i++) {
-         
+//     int k, a=1, b=0;
+//     for (int i = 0; i < 10; i++) {
+//     k=0;
+//     k=a+b;
+//     a=b;
+//     b=k;
+//     System.out.println(k);
+//     }
      
-     k=0;
-     k=a+b;
-     a=b;
-     b=k;
-     System.out.println(k);
-     }
-// int index =0;
-// while(true){
-// System.out.println(fibonacci(index));
-// index++;
-//   int num =0;
-//   int num2 = 1;
-//   int loop;
-//   int fibonacci;
+int index =0;
+ while(index<10){
+System.out.println(fibonacci(index));
+index++;
+  int num =0;
+  int num2 = 1;
+  int loop;
+  int fibonacci;
+ } 
+  
+//   }
    
-//    System.out.println(num);      
-////     for (loop= 0; loop <10; loop++) {
-//       fibonacci= num + num2;
-//      num =num2;
-//      num2 = fibonacci;
-//       System.out.println(fibonacci);
-////     }
-//    
         
         }
  
 
 
-// public static long fibonacci(int i ){
-// if(i==0) return 0;
-//  if(i<=2) return 1;
-// 
-//  long fibTerm = fibonacci(i-1)+ fibonacci(i-2);
-//   return fibTerm;
-//    }    
+ public static long fibonacci(int i ){
+ if(i==0) return 0;
+  if(i<=2) return 1;
+ 
+  long fibTerm = fibonacci(i-1)+ fibonacci(i-2);
+   return fibTerm;
+    }    
       
-}
 
+}
