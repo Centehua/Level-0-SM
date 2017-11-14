317543f1059fa48263cdc217cef0e08bd35d2264
Cenethua <Selenaminez@gmail.com>
2017-11-13 21:44:46 -0800

    Lil Change

--- src/RandomChar.java
+++ src/RandomChar.java
@@ -6,25 +6,47 @@ public class RandomChar {
  public static void main(String[] args) {
 		String userAns = JOptionPane.showInputDialog("Type in a character.");
 		Random z = new Random();
-	int num = z.nextInt(15+1)+5;
-	
+	int num = z.nextInt(16)+5;
 	trianglePrinter(userAns,num);
+  
+
+
+
+
 }
+ 
+ 
+ 
  static void trianglePrinter(String userAns, int num) {
 	
 
-	 	for (int a = 1; a <num-1; a++) {
+	for (int a = 1; a <num; a++) {
 			for (int i = 0; i < a; i++) {
 			System.out.print(userAns);	
 		}
 		System.out.println();	
 		
 		}
+        
+        
+        
+        for(int s = num; s > 0; s--) {
+           
+        for (int i =1; i < s; i++) {
+        System.out.print(userAns);
+    }
+   System.out.println();
+       
+     }
+       
+	
+           
+	
 		
-}
 		
 		 
  } 
- 
+}
+
 
 
